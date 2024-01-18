58dc401444821c643db4b4b414945f64c7c1d625
Eduardo Botelho <eduardo11224b@gmail.com>
2024-01-18 14:16:34 +0000

    feat: landing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import "./App.css";
 import DarkSection from "./components/DarkSection";
-import LightSection from "./components/LighSection";
+import LightSection from "./components/LightSection";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <DarkSection>A</DarkSection>
+      <DarkSection>
+        <Navbar />
+        <section id="landing">
+          <h1 className="big-heading">Say hello to ReactJS</h1>
+          <p>
+            You will learn how to use
+            <br /> the most popular frontend library,
+            <br /> and become a super Ninja developer.
+          </p>
+          <a className="button-white">Awesome!</a>
+        </section>
+      </DarkSection>
       <LightSection>B</LightSection>
     </div>
   );
