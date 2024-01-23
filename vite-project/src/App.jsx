43eae548006034c6e8a99161153f688c3ecf0c99
Pabloluis9 <140593315+Pabloluis9@users.noreply.github.com>
2024-01-23 20:53:26 +0100

    done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import './App.css';
+import TopSection from './components/TopSection/TopSection';
+import List from './components/List/List';
+
+function App() {
+  return (
+    <div className="App">
+      <TopSection />
+      <List />
+    </div>
+  );
+}
+
+export default App;
