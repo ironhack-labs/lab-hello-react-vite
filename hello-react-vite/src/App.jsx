a4092c7c2b82d54064cb8592fde333a4554a7d55
Filipa <filipamnmlopes@hotmail.com>
2024-05-27 16:21:50 +0100

    add header

--- hello-react-vite/src/App.jsx
+++ hello-react-vite/src/App.jsx
@@ -1,11 +1,13 @@
 // src/App.jsx
 import './App.css';
+import FourIcons from './components/FourIcons';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <FourIcons></FourIcons>
     </div>
   );
 }
