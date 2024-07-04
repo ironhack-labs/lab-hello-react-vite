c445c66e38764d4a8f8cc141c7e556ffe399447b
Bruno-Zamperoni <bzamperoni@gmai.com>
2024-07-04 10:08:33 -0300

    done

---
+++ lab-week1-day1/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div className="root"></div>
+  );
+}
+
+export default App;
---
+++ lab-week1-day1/src/header.jsx
@@ -0,0 +1,21 @@
+function header() {
+  return (
+    <div className="header">
+      <div className="logos">
+        <div>
+          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" alt="logo" />
+        </div>
+        <div>
+          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" alt="logo" />
+        </div>
+      </div>
+        <div className="text">
+            <h1>Say hello to ReactJS</h1>
+            <p>You will learn how to use the most popular frontend library, and become a super ninja developer</p>
+            <button>Awesome!</button>
+        </div>
+    </div>
+  );
+}
+
+export default header;
