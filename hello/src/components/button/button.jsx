2198046a02e8f4d8c3dcf119f57b77f77b390e0c
Ana Molina <ana713w@gmail.com>
2024-11-10 14:32:24 +0100

    done

---
+++ hello/src/components/button/button.jsx
@@ -0,0 +1,9 @@
+import "./button.css"
+
+function Button ({children}) {
+    return (
+        <button className="button">{children}</button>
+    )
+}
+
+export default Button;
---
+++ hello/src/components/paragraph/paragraph.jsx
@@ -0,0 +1,9 @@
+import "./paragraph.css"
+
+function Paragraph ({children}) {
+    return (
+        <p className="paragraph">{children}</p>
+    )
+}
+
+export default Paragraph;
