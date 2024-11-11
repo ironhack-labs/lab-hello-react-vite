962d0ba3b105c291c4938b62300eb9b39c5ac99b
machmaegal <aka.pleat371@passinbox.com>
2024-11-11 20:01:26 +0100

    init

---
+++ hello-React/src/App.jsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
